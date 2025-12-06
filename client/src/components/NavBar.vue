d9d1a24107601fe62c972772f72d6ffcf1b0da50
Anton Dettki <caspardett@gmail.com>
2025-12-06 11:15:00 +0100

    styling

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,9 +1,17 @@
 <template>
     <v-row class="my-4 mx-4">
-        <v-btn to="/" text="Bahn Thingy" variant="plain"/>
+        <v-btn to="/" text="Plan Bahn" variant="plain" class="planbahn-btn" />
     </v-row> 
 </template>
 
 <script setup>
   //
 </script>
+
+<style scoped>
+.planbahn-btn {
+  color: #EC0016 !important;
+  font-weight: bold !important;
+  font-size: 1.1rem;
+}
+</style>
